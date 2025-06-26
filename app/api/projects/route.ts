b9df82cbc0ce51a81dabedad72af0e97fa9c4e99
ghaleprachan <ghale.prachan@gmail.com>
2025-06-26 16:15:47 -0400

    add update project schema and POST method

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -6,6 +6,7 @@ const ProjectSchema = new mongoose.Schema({
   image: { type: String, required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
+  techStack: { type: [String], required: true }, // Array of technologies used
 });
 
 function getProjectModel() {
@@ -21,5 +22,26 @@ export async function GET() {
   } catch (error) {
     console.error("API error:", error);
     return NextResponse.json({ error: "Internal Server Error " + error }, { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const { image, title, description, techStack } = await req.json();
+    await connectToDatabase();
+    const Project = getProjectModel();
+    const project = await Project.create({
+      image,
+      title,
+      description,
+      techStack,
+    });
+    return NextResponse.json(project, { status: 201 });
+  } catch (error) {
+    console.error("API error:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error " + error },
+      { status: 500 }
+    );
   }
 }
